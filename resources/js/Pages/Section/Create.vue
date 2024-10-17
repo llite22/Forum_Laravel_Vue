65944f032280fb17a12154e67a56e2c3cd892b6c
llite22 <llite2@yandex.ru>
2024-10-18 01:52:20 +0300

    first commit

---
+++ resources/js/Pages/Section/Create.vue
@@ -0,0 +1,49 @@
+<script>
+import MainLayout from "@/Layouts/MainLayout.vue";
+import {Link} from "@inertiajs/vue3";
+
+export default {
+    name: "Create",
+
+    data() {
+        return {
+            title: ''
+        }
+    },
+
+    components: {
+        Link
+    },
+
+    methods: {
+        store() {
+            this.$inertia.post('/sections', {
+                title: this.title
+            })
+        }
+    },
+
+    layout: MainLayout
+}
+</script>
+
+<template>
+    <div>
+        <div class="flex flex-col gap-2 mb-4">
+            <h3 class="text-xl">Добавить раздел</h3>
+            <div>
+                <div class="mb-4">
+                    <input placeholder="Заголовок" type="text" v-model="title" class="border-gray-300 p-2 w-1/4">
+                </div>
+                <div>
+                    <a class="flex justify-center w-1/4 py-2 bg-sky-500 border border-sky-600 text-white text-center cursor-pointer"
+                       @click.prevent="store">Добавить</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ resources/js/Pages/Section/Index.vue
@@ -0,0 +1,27 @@
+<script>
+import MainLayout from "@/Layouts/MainLayout.vue";
+import {Link} from "@inertiajs/vue3";
+
+export default {
+    name: "index",
+
+    components: {
+        Link
+    },
+
+    layout: MainLayout
+}
+</script>
+
+<template>
+    <div>
+        <div class="flex items-center gap-2">
+            <h3 class="text-xl">Разделы</h3>
+            <Link :href="route('sections.create')" class="flex justify-center w-8 bg-white px-2 py-1 border border-gray-300 rounded-lg text-center">+</Link>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
